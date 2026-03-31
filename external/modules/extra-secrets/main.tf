51065dcf1d4c8fcb9521876091de17eeae0a0a86
Matt Alford <matt@malford.io>
2026-03-31 12:21:19 -0400

    migrate `kubernetes_secret` resources to `kubernetes_secret_v1` with state migration blocks

--- external/modules/extra-secrets/main.tf
+++ external/modules/extra-secrets/main.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_secret" "external" {
+resource "kubernetes_secret_v1" "external" {
   metadata {
     name      = var.name
     namespace = var.namespace
@@ -9,4 +9,11 @@ resource "kubernetes_secret" "external" {
   }
 
   data = var.data
+}
+
+# State migration: kubernetes_secret -> kubernetes_secret_v1
+# Safe to remove after first apply.
+moved {
+  from = kubernetes_secret.external
+  to   = kubernetes_secret_v1.external
 }
--- external/modules/ntfy/main.tf
+++ external/modules/ntfy/main.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_secret" "ntfy_auth" {
+resource "kubernetes_secret_v1" "ntfy_auth" {
   metadata {
     name      = "webhook-transformer"
     namespace = "monitoring-system"
@@ -12,4 +12,11 @@ resource "kubernetes_secret" "ntfy_auth" {
     NTFY_URL   = var.auth.url
     NTFY_TOPIC = var.auth.topic
   }
+}
+
+# State migration: kubernetes_secret -> kubernetes_secret_v1
+# Safe to remove after first apply.
+moved {
+  from = kubernetes_secret.ntfy_auth
+  to   = kubernetes_secret_v1.ntfy_auth
 }
